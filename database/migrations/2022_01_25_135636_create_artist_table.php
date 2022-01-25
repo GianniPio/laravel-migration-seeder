9615373746bf51f880e5f843d2de3a5be447e3c2
GianniPio <giannipiopellegrino00@gmail.com>
2022-01-25 15:07:36 +0100

    create table for artist

--- database/migrations/2022_01_25_135636_create_artist_table.php
+++ database/migrations/2022_01_25_135636_create_artist_table.php
@@ -15,6 +15,15 @@ class CreateArtistTable extends Migration
     {
         Schema::create('artist', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30);
+            $table->string('nickname', 30)->nullable();
+            $table->string('surname', 30);
+            $table->date('year_of_birth');
+            $table->string('birth_place', 50);
+            $table->integer('number_of_album');
+            $table->integer('number_of_songs');
+            $table->string('website', 30)->nullable();
+            $table->string('instagram', 30)->nullable();
             $table->timestamps();
         });
     }
